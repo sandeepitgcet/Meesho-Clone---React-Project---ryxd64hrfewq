538759762cee27b336111f770a27de0b4cccbfdf
ssssandeep <ssssandeep57@gmail.com>
2023-01-30 19:17:29 +0000

    updaetd

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -15,7 +15,7 @@ const LoginButton = styled(Button)(({ theme }) => ({
 
 const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const [user, setUser] = useState({email:'', password:''})
+    const [user, setUser] = useState({fname:'',lname:'', email:'', password:''})
     
     const handleSubmit = () => {
         console.log(user);
@@ -26,9 +26,10 @@ const SignUp = () => {
       <Box padding={2} sx={{ display: 'flex', flexWrap: 'wrap', flexDirection:'column', rowGap:'10px' }}>
       <Typography variant='h4' color={THEME_COLOR} sx={{display:'grid', placeItems:'center'}}>LOGIN</Typography>
       <Form onSubmit={handleSubmit} style={{ display: 'flex', flexWrap: 'wrap', flexDirection:'column', rowGap:'10px' }}>
-          
-          <TextField fullWidth label="Email" id='email' name='email' value={user.email} onChange={(e)=>setUser({...user, email:e.target.value})}/>
-          <TextField fullWidth label="Password" id='password' type={'password'} name='password' value={user.password} onChange={(e)=>setUser({...user, password:e.target.value})}/>
+          <TextField fullWidth label="First Name" value={user.fname} onChange={(e)=>setUser({...user, fname:e.target.value})}/>
+          <TextField fullWidth label="Last Name" value={user.lname} onChange={(e)=>setUser({...user, lname:e.target.value})}/>
+          <TextField fullWidth label="Email" value={user.email} onChange={(e)=>setUser({...user, email:e.target.value})}/>
+          <TextField fullWidth label="Password" type={'password'} value={user.password} onChange={(e)=>setUser({...user, password:e.target.value})}/>
           <LoginButton variant='contained' type='submit'>Login</LoginButton>
         
       </Form>
