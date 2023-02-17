11abd2fd708673c9b641c14339cbd287b5de0bad
ssssandeep <ssssandeep57@gmail.com>
2023-02-17 15:50:11 +0530

    Fixed fetchData()

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { Grid, Box } from '@mui/material'
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import useFetchProduct from '../hooks/useFetchProduct';
-import { useDispatch, useSelector } from 'react-redux';
-import { setProducts } from './../services/redux/productSlice'
-import AllProduct from '../components/AllProduct';
-import Filter from '../components/Filter';
-import Footer from './../components/Footer'
+import React, { useEffect, useState } from "react";
+import { Grid, Box } from "@mui/material";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import useFetchProduct from "../hooks/useFetchProduct";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "./../services/redux/productSlice";
+import AllProduct from "../components/AllProduct";
+import Filter from "../components/Filter";
+import Footer from "./../components/Footer";
+import axios from "axios";
 
 const Home = () => {
-    
-    const [isLoading, setLoading] = useState(false);
-    const dispatch = useDispatch();
+  const [isLoading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        setLoading(true)
-        useFetchProduct().then((res)=>{
-            dispatch(setProducts(JSON.parse(JSON.stringify(res.data))))
-        }).catch((error)=>{
-            console.log("error while fetching alll products "+ error);
-        }).finally(()=>{
-            setLoading(false);
-        })
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const products = await axios.get(
+          "https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/products"
+        );
+        dispatch(setProducts(JSON.parse(JSON.stringify(products.data))));
+      } catch (err) {
+        console.log("error while fetching alll products " + err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
-    },[])
+  return (
+    <React.Fragment>
+      <Grid container>
+        <Grid item sm={3} md={3}>
+          <Filter />
+        </Grid>
+        <Grid item xs={12} sm={9} md={9}>
+          <AllProduct />
+        </Grid>
+      </Grid>
+      <Footer />
+    </React.Fragment>
+  );
+};
 
-    return (
-        <React.Fragment>
-            <Grid container>
-                <Grid item sm={3} md={3}>
-                    <Filter />
-                </Grid>
-                <Grid item xs={12} sm={9} md={9} >
-                    <AllProduct/>
-                </Grid>
-                
-            </Grid>
-            <Footer />
-        </React.Fragment>
-    )
-}
-
-
-export default Home
+export default Home;
